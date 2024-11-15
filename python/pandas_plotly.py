6773c55b73f6af437e4b57a7851ebebcb9c63ef1
awentity <107478986+awentity@users.noreply.github.com>
2024-11-15 15:22:32 +0000

    Add basic filtering to graph
    Show open vs sma

--- python/pandas_plotly.py
+++ python/pandas_plotly.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     csv_panda = read_csv(Path("../data/Ethereum_13_11_2023-12_11_2024_historical_data_coinmarketcap.csv"))
     print(csv_panda.info())
     csv_panda["SMA"] = csv_panda["open"].rolling(100).mean()
-    figure = csv_panda.loc['2023-11-21T00:00:00.000Z':].plot(title='ETH/USD exchange rate', figsize=(10, 6))
+    figure = csv_panda.filter(items=["SMA", "open"]).loc['2023-11-21T00:00:00.000Z':].plot(title='ETH/USD exchange rate', figsize=(10, 6))
     figure.get_figure().savefig("eth_usd.pdf")
