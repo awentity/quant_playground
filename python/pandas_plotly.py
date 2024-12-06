59dd0b7dfa3453ac26f6dae6596cdad3b47b88d9
awentity <107478986+awentity@users.noreply.github.com>
2024-12-06 07:29:40 +0000

    Update pandas_plotly.py

--- python/pandas_plotly.py
+++ python/pandas_plotly.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-
 import pandas as pd
 
 def read_csv(filepath: Path):
@@ -8,7 +7,6 @@ def read_csv(filepath: Path):
 
 if __name__ == "__main__":
     csv_panda = read_csv(Path("../data/Ethereum_13_11_2023-12_11_2024_historical_data_coinmarketcap.csv"))
-    print(csv_panda.info())
     csv_panda["SMA"] = csv_panda["open"].rolling(100).mean()
     figure = csv_panda.filter(items=["SMA", "open"]).loc['2023-11-21T00:00:00.000Z':].plot(title='ETH/USD exchange rate', figsize=(10, 6))
     figure.get_figure().savefig("eth_usd.pdf")
